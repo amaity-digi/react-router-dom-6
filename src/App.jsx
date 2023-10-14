d5656457c9a1dd429b7857c7be4d0d3d1f07821a
amaity-digi <116334087+amaity-digi@users.noreply.github.com>
2023-10-14 10:34:56 +0530

    feat(component): implemented vanlife project

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@ import Home from "./pages/Home";
 function App() {
   return (
     <BrowserRouter>
+    <header>
+    <Link to="/">VanLife</Link>
     <nav>
-      <Link to="/">Home</Link>
       <Link to="/about">About</Link>
     </nav>
+    </header>
+   
     <Routes>
      <Route path="/" element={<Home />}/>
      <Route path="/about" element={<About />}/>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,26 @@
+import { Link } from "react-router-dom";
 
 function About() {
   return (
-    <div>We are in About Pages</div>
-  )
+    <div>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGM-xTiGoilyYd-cxlINGWzrIIMg7pV9m90w&usqp=CAU"
+        alt=""
+      />
+      <div>
+      <h2>Typography Eat Sleep Travel Repeat, Adventure, Phrase, Mountain</h2>
+      <p>
+        Adventure travel. Adventure travel Backpacking Vacation Love,
+        Travel, backpack, adventure, magico png thumbnail Adventure travel
+        Backpacking
+      </p>
+      </div>
+      <div>
+        <h2>Your destination is waiting. <br/> Your van is ready.</h2>
+        <Link to="/vans">Explore our vans</Link>
+      </div>
+    </div>
+  );
 }
 
-export default About
+export default About;
